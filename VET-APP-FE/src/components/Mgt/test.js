9bf87d5ea70b1540020105466ca3cf4519293a69
davidguo <yguo@niricson.com>
2021-12-14 20:47:54 -0700

    add search

--- VET-APP-FE/src/components/Mgt/test.js
+++ VET-APP-FE/src/components/Mgt/test.js
@@ -1,31 +1,32 @@
 import { Row,Col,Space, Button,Popconfirm,message,Form,Select,Modal,Input,Spin,Pagination  } from 'antd'
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 import React, { Fragment } from 'react'
 import Transition from '../Static/Transition'
 import { Table } from 'antd';
 import { useHistory } from "react-router-dom";
-import { SearchOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import axios from '../Api/request';
 import moment from 'moment';
-import { ROLE_ANIMALHTTECH,TREATMENT,PROCESSING,PENDING,ROLE_ADMIN,ROLE_STUDENT,ROLE_TEACHINGTECH,ROLE_ANIMALCAREAT,dailyStatusData } from '../DummyData/dummy';
+import { TREATMENT,PROCESSING,PENDING,ROLE_ADMIN,ROLE_STUDENT,ROLE_TEACHINGTECH,ROLE_ANIMALCAREAT,statusData } from '../DummyData/dummy';
 import { useForm } from 'antd/lib/form/Form';
 
 
-const AniamlMgt = () => {
+
+
+const test = () => {
       const { Option } = Select;
       const [animalData, setAnimalData] = useState([]);
       const [userData, setUserData] = useState([]);
       const [reqData, setReqData] = useState([]);
       const [isModalVisible, setIsModalVisible] = useState(false);
       const [isTRVisible, setIsTRVisible] = useState(false);
-      const [isDRVisible, setIsDRVisible] = useState(false);
       const [loading, setloading] = useState(true);
       const [pageNum, setpageNum] = useState(1);
       const [elementTotal, setelementTotal] = useState(0);
       let history = useHistory();
       const [commentForm]=useForm();
       const [treatmentCommentForm]=useForm();
-      const [dailyReportForm]=useForm();
       const [data, setData] = useState([]);
       const [animalId, setanimalId] = useState(0);
       const [techId, settechId] = useState(0);
@@ -72,18 +73,14 @@ const AniamlMgt = () => {
       const handleOk = () => {
         setIsModalVisible(false);
       };
-      const handleDROk = () => {
-        setIsDRVisible(false);
+    
+      const handleCancel = () => {
+        setIsModalVisible(false);
       };
       const handleTROk = () => {
         setIsTRVisible(false);
       };
-      const handleCancel = () => {
-        setIsModalVisible(false);
-      };
-      const handleDRCancel = () => {
-        setIsDRVisible(false);
-      };
+    
       const handleTRCancel = () => {
         setIsTRVisible(false);
       };
@@ -101,21 +98,18 @@ const AniamlMgt = () => {
          message.success("req animal successfully");
        })
     }
-    const handleDailyReport = (record) => {
-      setanimalId(record.id);
-      //settechId(record.requestTo);
-      setIsDRVisible(true);
-      //reqTreatment(,, record.description);
-    };
-
     const handleTreatmentRequest = (record) => {
       setanimalId(record.id);
       settechId(record.requestTo);
       setIsTRVisible(true);
       //reqTreatment(,, record.description);
     };
+/*
+    function reqTreatment(animalID,userID, description) {
+      setIsTRVisible(true);
 
-
+    }*/
+    //console.log(data);
     function setUnavailableStatus(key){
       axios.get("api/animal/setUnavailableStatus?id="+key)
       .then(res=>{
@@ -146,7 +140,7 @@ const AniamlMgt = () => {
        treatmentCommentForm.resetFields();
      })
 
-      /*
+/*
       axios.post("api/comment/addComment",{...values, description:values.description, animalId:animalId,userId:localStorage.getItem("userId")})
       .then(res=>{  
         message.success(res.data.message);
@@ -154,24 +148,8 @@ const AniamlMgt = () => {
         treatmentCommentForm.resetFields();
       })*/
     };
-    const onFinishDR = (record) => {
-      let now = moment().format('YYYY-MM-DD');
-      console.log(animalId + " " + localStorage.getItem("userId") + " " + record.description +" " +record.location + " " +  record.status)
-      let params = { animalId: animalId, userId: localStorage.getItem("userId"),
-      description:record.description, location: record.location, status: record.status }
-      axios.get("api/dailyReport/add", { params})
-     .then(res=>{
-       console.log(res.data.message);  
-       message.success("daily report added successfully");
-       setIsDRVisible(false);
-       dailyReportForm.resetFields();
-     })
-    };
-    
+  
     const onFinishFailedTreatmentReq = (errorInfo) => {
-      
-    };
-    const onFinishFailedDR = (errorInfo) => {
       
     };
     const handleChange = (value,v1,record) => {
@@ -187,25 +165,19 @@ const AniamlMgt = () => {
     function loadAnimal(pageNum){
       setloading(true);
       setpageNum(pageNum);
-      if(localStorage.getItem("role")==ROLE_ADMIN||localStorage.getItem("role")==ROLE_ANIMALCAREAT||localStorage.getItem("role")==ROLE_ANIMALHTTECH){
-        //console.log("test");
-        
-        axios.get('api/animal/getAllAnimal?pageNum='+pageNum)
-        .then((res) => {
-          //console.log("res.data.data");
-          //console.log(res.data.data);
-          setelementTotal(res.data.pageTotal);
-          setAnimalData(res.data.data); 
-          setloading(false);
-        })
+      if(localStorage.getItem("role")==ROLE_ADMIN||localStorage.getItem("role")==ROLE_ANIMALCAREAT){
+        axios.get('api/animal/getAllAnimal?pageNum='+pageNum).then((res) => {
+        setelementTotal(res.data.pageTotal);
+        setAnimalData(res.data.data); 
+        setloading(false);
+      })
       }
       else{
         axios.get("api/animal/getAvailableAnimal")
-        .then(res=>{
-          console.log("res.data.data");
-          setAnimalData(res.data.data);  
-          setloading(false);
-        })
+      .then(res=>{
+        setAnimalData(res.data.data);  
+        setloading(false);
+      })
       }  
     }
 
@@ -229,10 +201,14 @@ const AniamlMgt = () => {
     const onFinishFailed = (errorInfo) => {
       
     };
+
+
+
+   
     const columns = [
         {
           title: 'ID',
-          dataIndex: 'id',
+          dataIndex: 'id'
           
         },
         {
@@ -298,7 +274,7 @@ const AniamlMgt = () => {
           render:(text,record) => (
             localStorage.getItem("role")==ROLE_TEACHINGTECH ||  localStorage.getItem("role")==ROLE_ANIMALCAREAT?
               (<Select
-              placeholder="Technician ID"
+              placeholder="Select a health technician"
               onChange={(e)=>handleChange(e, text, record)}
               allowClear
               >
@@ -316,12 +292,11 @@ const AniamlMgt = () => {
           render: (text, record) => (
             <Space size="middle">
               {localStorage.getItem("role")==ROLE_TEACHINGTECH &&  <Button onClick={() => handleAnimalRequest(record)}>Request Animal</Button>}
-              {localStorage.getItem("role")==ROLE_ANIMALCAREAT && record.status !="Treatment" && <Button onClick={() => handleTreatmentRequest(record)}>Req Treatment</Button>}
-              {localStorage.getItem("role")==ROLE_ANIMALCAREAT && <Button onClick={() => handleDailyReport(record)}>Add DailyReport</Button>}
-              {localStorage.getItem("role")!=ROLE_STUDENT && localStorage.getItem("role")!=ROLE_TEACHINGTECH &&<Button onClick={() => editAnimal(record.key)}>Edit</Button>}
-              {<Button onClick={() => commentAnimal(record.key)}>Comment</Button>}
+              {localStorage.getItem("role")==ROLE_ANIMALCAREAT && record.status !="Treatment" && <Button onClick={() => handleTreatmentRequest(record)}>Request Treatment</Button>}
+              <Button onClick={() => editAnimal(record.key)}>Edit</Button>
+              {localStorage.getItem("role")==ROLE_STUDENT && <Button onClick={() => commentAnimal(record.key)}>Comment</Button>}
               {localStorage.getItem("role")==ROLE_ADMIN  && <Button onClick={() => viewComments(record.key)}>View Comments</Button>} 
-              {<Button onClick={() => viewAnimalProfile(record.key)}>View Profile</Button>}
+              {localStorage.getItem("role")==ROLE_STUDENT && <Button onClick={() => viewAnimalProfile(record.key)}>View Profile</Button>}
               <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
               <Form 
                   form={commentForm}
@@ -382,68 +357,6 @@ const AniamlMgt = () => {
                       
                   </Form>
               </Modal>
-              <Modal title="Daily Report" visible={isDRVisible} onOk={handleDROk} onCancel={handleDRCancel}>
-              <Form 
-                  form={dailyReportForm}
-                  onFinish={onFinishDR}
-                  onFinishFailed={onFinishFailedDR}
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 16 }}
-                  name="dynamic_rule">
-
-
-                      <Form.Item
-                          name="description"
-                          label="description"
-                          rules={[
-                          {
-                              required: true,
-                              message: 'Please input description',
-                          },
-                          ]}
-                      >
-                          <Input placeholder="Please input description" />
-                      </Form.Item>
-                      <Form.Item
-                          name="location"
-                          label="location"
-                          rules={[
-                          {
-                              required: true,
-                              message: 'Please input location',
-                          },
-                          ]}
-                      >
-                          <Input placeholder="Please input location" />
-                      </Form.Item>
-                      <Form.Item
-                          name="status"
-                          label="status"
-                          rules={[
-                          {
-                              required: true,
-                              message: 'Please select a status',
-                          },
-                          ]}
-                      >
-                          <Select
-                          placeholder="Select a status"
-                          allowClear
-                          >
-                          {dailyStatusData.map(rl =>(
-                              <Option key={rl.id} value={rl.stsName}>{rl.stsName}</Option>
-                          ))}
-                          </Select>
-                      </Form.Item>
-
-                      <Form.Item  wrapperCol={{ offset: 6, span: 16 }}>
-                          <Button type="primary" htmlType="submit">
-                          Submit
-                          </Button>
-                      </Form.Item>
-                      
-                  </Form>
-              </Modal>
             </Space>
           ),
         },
@@ -458,13 +371,13 @@ const AniamlMgt = () => {
         <Transition></Transition>
         </Col>
         <Col span={16} style={{marginTop:"20px"}}>
+          
            <h1>Animal Management</h1>
            {loading && <Spin tip="Loading..."/>}
            {!loading &&  
-             <Fragment>
+            <Fragment>
            <Table 
-            pagination={false}
-           
+           pagination={false}         
            bordered columns={columns} dataSource={data} />
            <div  style={{marginTop:"10px"}}></div>
            <Pagination 
@@ -475,9 +388,14 @@ const AniamlMgt = () => {
            showTotal={()=>{
             return 'total of ' + elementTotal + ' data'; 
            }}/>
-           </Fragment>}
+           </Fragment>
+           
+           }
+           
         </Col>
+
         </Row>
+
         </React.Fragment>
     )
 }
